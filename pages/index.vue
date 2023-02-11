674656576b57d0cdf4bfa082150337bb965d5f75
Umar Odeh Adejoh <stradox@gmail.com>
2023-02-12 00:54:32 +0100

    display raw data in view - good start

--- pages/index.vue
+++ pages/index.vue
@@ -3,17 +3,38 @@ import { storeToRefs } from 'pinia';
 import { useEventStore } from '../stores/eventStore';
 
 const eventStore = useEventStore();
-const {getEvents: events} = storeToRefs(eventStore);
+const { getEvents: events } = storeToRefs(eventStore);
 
 watch(events, (newVal) => {
-  console.log(newVal);
-}, {immediate: true});
+  console.log({ events: newVal });
+})
 
-onMounted(async () => {
-
+const filteredEvents = computed(() => {
+  return events.value?.map((event) => {
+    const attendees = event.attendees.filter((attendee) => {
+      return attendee.responseStatus === 'accepted';
+    }).map((el) => el.email);
+    return {
+      id: event.id,
+      attendees,
+      description: event.description || '',
+      organizer: event.organizer.email,
+      start: event.start,
+      summary: event.summary,
+    };
+  })
 })
 </script>
 
 <template>
-  <div>Index page!</div>
+  <ul>
+    <li v-for="event in filteredEvents" :key="event.id">
+      <span>Organizer: {{ event.organizer }}</span><br />
+      <span>Description: {{ event.description }}</span><br />
+      <div>Attendees:</div>
+      <p v-for="attendee in event.attendees" :key="attendee">{{ attendee }}</p>
+      <span>Summary: {{ event.summary }}</span>
+      <hr/>
+    </li>
+  </ul>
 </template>
