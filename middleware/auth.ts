4c76b530060c4c48c7213e764899da8d3937bd13
Umar Odeh Adejoh <stradox@gmail.com>
2023-02-18 02:56:27 +0100

    modified auth routeguard to dump you to index route if not logged in

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -5,7 +5,7 @@ import { useUserStore } from "~~/stores/userStore"
 export default defineNuxtRouteMiddleware(async (to, from) => {
   const userStore = useUserStore();
   const { isLoggedIn } = storeToRefs(userStore);
-  if (!isLoggedIn.value && to.fullPath !== from.fullPath) {
-    return navigateTo(from.fullPath);
+  if (!isLoggedIn.value) {
+    return navigateTo({name: 'index'});
   }
 });
